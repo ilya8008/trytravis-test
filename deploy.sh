722c964b7a1152d7bbf68e3454f538c21468949d
Ilya Ivanov <ilya@8008.su>
2018-10-20 10:29:36 +0300

    Add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# clone app code
+git clone -b monolith https://github.com/express42/reddit.git
+#
+# install dependencies
+cd reddit && bundle install
+#
+# start app server
+puma -d
